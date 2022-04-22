7f6e9178aca0ba50318d9d161d2e11eff6a5fd5d
Miller <milleri@wwt.com>
2022-04-22 10:04:58 -0500

    adds header to liars dollars

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './Header.css'
+
+function Header() {
+    return (
+        <div className={'header'}>
+            <header className={'liars-dollar'}>
+                <h1>Liars Dollars</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
