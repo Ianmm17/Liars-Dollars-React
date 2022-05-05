4e5528ab70ceec32b1f50c59f6e9ca2ca33fb14b
Ian Miller <ianmm17@yahoo.com>
2022-05-05 09:26:42 -0500

    refactor, removes curly braces and not needed prop

--- src/components/PlayerCardContainer/PlayerCard.js
+++ src/components/PlayerCardContainer/PlayerCard.js
@@ -19,10 +19,10 @@ function PlayerCard(props) {
 
     return (
         <form className="player-card">
-            <CustomInput type="text" classes={'input-name'} name={props.name} changeHandler={nameHandler} placeHolder='Enter Name' />
-            <CustomInput type="number" classes={'input-balance'} balance={props.balance}  changeHandler={balanceHandler} placeHolder='Enter starting balance' />
-            <CustomButton winOrLost={'W'} classes={'winner'} player={{name: props.name, balance: props.balance}}/>
-            <CustomButton winOrLost={'L'} classes={'lost'}/>
+            <CustomInput type="text" classes='input-name' changeHandler={nameHandler} placeHolder='Enter Name' />
+            <CustomInput type="number" classes='input-balance' changeHandler={balanceHandler} placeHolder='Enter starting balance' />
+            <CustomButton winOrLost='W' classes='winner' player={{name: props.name, balance: props.balance}}/>
+            <CustomButton winOrLost='L' classes='lost'/>
         </form>
     )
 }
