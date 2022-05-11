c661ba240dd1f154bd3964600c12f9c52b1b6130
Ian Miller <ianmm17@yahoo.com>
2022-05-11 10:38:45 -0500

    adds prop to CustomInput component to determine what display text should be shown.

--- src/components/PlayerCardContainer/CustomInput.js
+++ src/components/PlayerCardContainer/CustomInput.js
@@ -1,11 +1,11 @@
-import React, {useState} from "react";
+import React from "react";
 
 import './CustomInput.css'
 
 function CustomInput(props) {
         return (
             <input type={props.type} className={props.classes} placeholder={props.placeHolder}
-                   value={props.name? props.name : props.balance} onChange={props.changeHandler}
+                   value={props.displayedText} onChange={props.changeHandler}
             />
         )
 }
--- src/components/PlayerCardContainer/PlayerCardForm.js
+++ src/components/PlayerCardContainer/PlayerCardForm.js
@@ -26,8 +26,8 @@ function PlayerCardForm(props) {
 
     return (
         <form className="player-card">
-            <CustomInput type="text" classes='input-name' name={props.player.name} changeHandler={nameHandler} gameStarted={props.gameStarted} placeHolder='Enter Name' />
-            <CustomInput type="number" classes='input-balance' balance={props.player.balance}  changeHandler={balanceHandler} gameStarted={props.gameStarted} placeHolder='Enter starting balance' />
+            <CustomInput type="text" classes='input-name' name={props.player.name} displayedText={props.player.name} changeHandler={nameHandler} gameStarted={props.gameStarted} placeHolder='Enter Name' />
+            <CustomInput type="number" classes='input-balance' balance={props.player.balance} displayedText={props.player.balance} changeHandler={balanceHandler} gameStarted={props.gameStarted} placeHolder='Enter starting balance' />
             <CustomButton winOrLost='W' classes='winner' player={{name: props.name, balance: props.balance}} updatePlayersBalance={updatePlayersBalance} startGame={startGame} gameStarted={props.gameStarted}/>
             <CustomButton winOrLost='L' classes='lost'/>
         </form>
