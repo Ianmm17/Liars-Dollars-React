5ccd20b66b08bf64012daaa238f127179a11498a
Ian Miller <ianmm17@yahoo.com>
2022-05-11 10:10:40 -0500

    changed label element to a paragraph element

--- src/components/PlayerCardContainer/PlayerCardForm.js
+++ src/components/PlayerCardContainer/PlayerCardForm.js
@@ -6,8 +6,8 @@ import CustomButton from "./CustomButton";
 function PlayerCardForm(props) {
     return (
         <div className={'player-card-display'}>
-            <label className={'player-name-display'}>{props.player.name}</label>
-            <label className={'player-balance-display'}>${props.player.balance}.00</label>
+            <p className={'player-name-display'}>{props.player.name}</p>
+            <p className={'player-balance-display'}>${props.player.balance}.00</p>
             <CustomButton winOrLost='W' classes='winner' player={{name: props.name, balance: props.balance}} gameStarted={props.gameStarted}/>
             <CustomButton winOrLost='L' classes='lost'/>
         </div>
